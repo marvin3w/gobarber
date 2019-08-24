ef9b6dbb40ea535b6b7bc145803e5486819640dc
marvin3w <marvin.3w@gmail.com>
2019-08-24 19:15:46 -0300

    Up to fix bug

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-// import { async } from 'rxjs/internal/scheduler/async';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Marcus Oliveira',
-    email: 'marvin.3w@gmail.com',
-    password_hash: '123456',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
