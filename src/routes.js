8d207649db832f9c74d8e4191ed8434ea9d5a892
marvin3w <marvin.3w@gmail.com>
2019-08-24 13:15:47 -0300

    create loader models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+// import { async } from 'rxjs/internal/scheduler/async';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res)=>{
- return res.json({message: 'Hello World'});
-})
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Marcus Oliveira',
+    email: 'marvin.3w@gmail.com',
+    password_hash: '123456',
+  });
+  return res.json(user);
+});
 
 export default routes;
