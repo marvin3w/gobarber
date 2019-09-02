a04c36c3458f75679af4f9188c68f809d01c539a
marvin3w <marvin.3w@gmail.com>
2019-09-01 23:17:07 -0300

    Create database mongo to notifications

--- src/app/controllers/AppointementController.js
+++ src/app/controllers/AppointementController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointementController {
   async index(req, res) {
@@ -62,9 +64,9 @@ class AppointementController {
     /**
      * Check for past dates
      */
-    const hourStar = startOfHour(parseISO(date));
+    const hourStart = startOfHour(parseISO(date));
 
-    if (isBefore(hourStar, new Date())) {
+    if (isBefore(hourStart, new Date())) {
       return res.status(400).json({ error: 'Past dates are not permitted' });
     }
 
@@ -75,7 +77,7 @@ class AppointementController {
       where: {
         provider_id,
         canceled_at: null,
-        date: hourStar,
+        date: hourStart,
       },
     });
 
@@ -89,6 +91,22 @@ class AppointementController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
